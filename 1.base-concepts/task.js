c005423063e216d3d333be974cfeedefc4819691
exe356 <lexxxs@mail.ru>
2023-06-16 16:07:16 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  const discriminant = b ** 2 - 4 * a * c;
+  let result = [];
+
+  if (discriminant < 0) {
+    // No roots
+    return result;
+  } else if (discriminant === 0) {
+    // One root
+    result.push(-b / (2 * a));
+  } else {
+    // Two roots
+    result.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    result.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  }
+
+  return result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (typeof percent !== 'number' || typeof contribution !== 'number' || typeof amount !== 'number' || typeof countMonths !== 'number') {
+    return false;
+  }
+
+  percent = percent / 100; 
+  const loanBody = amount - contribution; 
+  const monthlyPercent = percent / 12; 
+  const exponent = Math.pow(1 + monthlyPercent, countMonths); 
+  const monthlyPayment = loanBody * (monthlyPercent + monthlyPercent / (exponent - 1)); 
+  const totalAmount = monthlyPayment * countMonths; 
+
+  return +totalAmount.toFixed(2); 
 }
