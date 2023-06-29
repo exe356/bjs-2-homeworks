def74c82984ccf38fb3e7715fa4677501fabb237
exe356 <lexxxs@mail.ru>
2023-06-29 16:17:43 +0300

    Восьмое задание готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,44 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash == hash);
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    }
+
+    let result = func(...args);
+    cache.push({
+      'hash': hash,
+      'value': result
+    });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
+
+function debounceDecoratorNew(func, ms) {
+  let timer;
   
+  function wrapper(...args) {
+    wrapper.allCount++;
+    if (!timer) {
+    func(...args);
+    wrapper.count++;
+  }
+      clearTimeout(timer);
+    timer = setTimeout(() => {func(...args); 
+    wrapper.count ++}, ms);
+  }
+    
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
 }
